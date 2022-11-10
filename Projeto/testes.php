5f95b9ab53dddf03e3a8f2e0c643f96f88820981
walmarley22@hotmail.com <walmarley22@hotmail.com>
2022-11-10 15:54:23 -0300

    projeto estudos de DDD

---
+++ Projeto/testes.php
@@ -0,0 +1,25 @@
+<?php
+
+use Alura\DesignPattern\CalculadoraDeDescontos;
+use Alura\DesignPattern\CalculadoraDeImpostos;
+use Alura\DesignPattern\Impostos\{Icms, Iss};
+use Alura\DesignPattern\Orcamento;
+
+require 'vendor/autoload.php';
+
+// $calculadora = new CalculadoraDeImpostos();
+
+// $orcamento = new Orcamento();
+// $orcamento->valor = 100;
+
+// echo $calculadora->calcula($orcamento, new ICMS());
+// echo "\n";
+
+$calculadora = new CalculadoraDeDescontos();
+
+$orcamento = new Orcamento();
+$orcamento->valor = 600;
+$orcamento->quantidadeItens = 50;
+
+echo $calculadora->CalculaDescontos($orcamento);
+echo "\n";
