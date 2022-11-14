5973771dab9d404159133d583196694881d66571
walmarley22@hotmail.com <walmarley22@hotmail.com>
2022-11-14 13:48:07 -0300

    configurando proxy

---
+++ Projeto/src/Relatorio/OrcamentoZip.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\DesignPattern\Relatorio;
+
+use Alura\DesignPattern\Orcamento;
+
+class OrcamentoZip implements ConteudoExportado
+{
+    private Orcamento $orcamento;
+
+    public function __construct(Orcamento $orcamento)
+    {
+        $this->orcamento = $orcamento;
+    }
+
+    public function conteudo(): array
+    {
+        return [
+            'valor' => $this->orcamento->valor,
+            'quantidade_itens' => $this->orcamento->quantidadeItens
+        ];
+    }
+}
